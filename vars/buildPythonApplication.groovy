09d1e3e376742741051c78f5ddc1c1d2ddb3cbcb
JakubRATEART <your_email@example.com>
2026-06-01 11:50:01 +0000

    fix

--- vars/buildPythonApplication.groovy
+++ vars/buildPythonApplication.groovy
@@ -42,20 +42,6 @@ def call(Map config = [:]) {
 		    }
     	        }
 	    }
-            stage('Deploy') {
-                steps {
-                    script {
-                        // Dynamically compute the staging vs production ports in one place!
-                        def containerName = "app-${BRANCH_NAME}-${appName}"
-                        def hostPort = (BRANCH_NAME == 'main') ? '8000' : '8001'
-
-                        echo "Deploying ${containerName} to host port ${hostPort}..."
-                        sh "docker stop ${containerName} || true"
-                        sh "docker rm ${containerName} || true"
-                        sh "docker run -d --name ${containerName} -p ${hostPort}:${internalPort} ${appName}:latest"
-                    }
-                }
-            }
         }
 
         post {

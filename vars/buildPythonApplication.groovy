c664a2d6b7ee0f4c2e39f0cb19387f64b5b9a36c
JakubRATEART <your_email@example.com>
2026-06-03 10:29:01 +0000

    Jfilefix

--- vars/buildPythonApplication.groovy
+++ vars/buildPythonApplication.groovy
@@ -41,7 +41,7 @@ def call(Map config = [:]) {
 			sh "docker run -d --name ${containerName} -p ${hostPort}:${internalPort} ${appName}:latest"
 			echo "Verifying API availability on port ${hostport}"
 			sh "sleep 5"
-			sh "curl --fail http:/localhosts:${hostPORT}/api/status || (docker logs ${containerName} && exit 1)"
+			sh "curl --fail http:/localhosts:${hostPort}/api/status || (docker logs ${containerName} && exit 1)"
                     }
                 }
             }
